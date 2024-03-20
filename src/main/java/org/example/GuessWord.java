7b20eb516187a4ebd7d08ae8d3b151046016b52c
Giovanni Silva <no-reply@gsilva.pro>
2024-03-20 19:39:07 -0300

    Aula 08 - alternative implementation for letterExist

--- src/main/java/org/example/GuessWord.java
+++ src/main/java/org/example/GuessWord.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GuessWord {
     private final String word;
@@ -12,19 +13,21 @@ public class GuessWord {
 
     /**
      * Return the position of the letter in the word or empty array if not found
+     *
      * @param letter The letter to check
      * @return Position of the letter
      */
     public Integer[] letterExist(char letter) {
         List<Integer> letterPositions = new ArrayList<>();
 
-        for(int index = 0; index < this.word.length(); index++) {
-           char possibleLetter = this.word.charAt(index);
-           if(possibleLetter == letter) {
-               letterPositions.add(index);
-           }
-        }
-//        char possibleLetter = word.charAt(2);
+        IntStream.range(0, word.length())
+                .forEach(index -> {
+                    char possibleLetter = word.charAt(index);
+                    if (possibleLetter == letter) {
+                        letterPositions.add(index);
+                    }
+                });
+
         return letterPositions.toArray(Integer[]::new);
     }
 }
