199c653d34116e5c88a3745b141fa7cba639cc0b
Giovanni Silva <no-reply@gsilva.pro>
2024-03-19 22:23:11 -0300

    Aula 08 - high level main instructions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // criar um menu de opcoes
+        // perguntar qual o tamanho da palavra
+        // carregar uma palavra aleatoria
+        // adivinhar as letras
+        // contar as tentativas
+        // mostrar a advinhação
+        // finalizar o jogo com vitoria ou derrota
         System.out.println("Hello world!");
     }
 }
